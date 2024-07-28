01e3652c632fc46fdc8256d0d0a0ec7615e467f6
Christian Stenger <cristianstenger@gmail.com>
2024-07-28 03:08:31 -0400

    go

--- app/app.go
+++ app/app.go
@@ -8,10 +8,10 @@ import (
 	"os"
 	"strings"
 
-	"github.com/kudrykv/latex-yearly-planner/app/components/page"
-	"github.com/kudrykv/latex-yearly-planner/app/compose"
-	"github.com/kudrykv/latex-yearly-planner/app/config"
-	"github.com/kudrykv/latex-yearly-planner/app/tpls"
+	"github.com/cristenger/latex-kindle-planner/app/components/page"
+	"github.com/cristenger/latex-kindle-planner/app/compose"
+	"github.com/cristenger/latex-kindle-planner/app/config"
+	"github.com/cristenger/latex-kindle-planner/app/tpls"
 	"github.com/urfave/cli/v2"
 )
 
